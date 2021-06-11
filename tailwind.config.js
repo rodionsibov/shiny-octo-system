210965d2aff96dbbceb2c624d8c22c78aaaced62
Rodion Sibov <rodionsibov@gmail.com>
2021-06-11 22:05:47 +0300

    style: add extend animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blob: "blob 2s infinite"
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "scale(1)"
+          },
+          "33%": {
+            transform: "scale(1.1)"
+          },
+          "66%": {
+            transform: "scale(0.9)"
+          },
+          "100%": {
+            transform: "scale(1)"
+          },
+
+        }
+      }
+    },
   },
   variants: {
     extend: {},
